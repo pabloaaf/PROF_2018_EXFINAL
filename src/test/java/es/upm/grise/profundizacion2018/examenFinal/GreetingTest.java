15f6d0f2dfe5214c88edbd62932de87c0010b4cc
pabloaaf <palvaf02@estudiantes.unileon.es>
2019-01-21 15:25:20 +0100

    pasar nueva dependencia en los test y comprobar que funcionan

--- src/test/java/es/upm/grise/profundizacion2018/examenFinal/GreetingTest.java
+++ src/test/java/es/upm/grise/profundizacion2018/examenFinal/GreetingTest.java
@@ -1,27 +1,37 @@
 package es.upm.grise.profundizacion2018.examenFinal;
 
 import static org.junit.Assert.*;
-
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 public class GreetingTest {
-
+	private MyCalendar mockMyCalendar;
+	
+	@Before
+ 	public void setUp(){
+ 		mockMyCalendar = Mockito.mock(MyCalendar.class);
+ 		for(int i=13; i<25;i++){
+ 	 		Mockito.when(mockMyCalendar.get(i)).thenReturn(10); 			
+ 		}
+ 	}
+	
 	@Test
 	public void smokeTest1() {
 		Greeting greeting = new Greeting();
-		assertEquals("Good morning", greeting.getGreeting(null));
+		assertEquals("Good morning", greeting.getGreeting(null, mockMyCalendar));
 	}
 	
 	@Test
 	public void smokeTest2() {
 		Greeting greeting = new Greeting();
-		assertEquals("Good morning", greeting.getGreeting(Language.ENGLISH));
+		assertEquals("Good morning", greeting.getGreeting(Language.ENGLISH, mockMyCalendar));
 	}
 	
 	@Test
 	public void smokeTest3() {
 		Greeting greeting = new Greeting();
-		assertEquals("Buenos días", greeting.getGreeting(Language.SPANISH));
+		assertEquals("Buenos días", greeting.getGreeting(Language.SPANISH, mockMyCalendar));
 	}
 
 }
